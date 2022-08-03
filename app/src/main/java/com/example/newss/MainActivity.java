1d070993c1cef44da59c5d9b6cf29ba21a216dea
prashant_sriv <prashantedu2001@gmail.com>
2022-08-03 21:19:07 +0530

    Newss Commit

---
+++ app/src/main/java/com/example/newss/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.newss;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    TabItem mhome, mscience, mhealth, mtech, mentertainment, msports;
+    PagerAdapter pagerAdapter;
+    Toolbar mtoolbar;
+
+    String api = "e0ef2052377440d2a8968408bae60732";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.notibarcolor, this.getTheme()));
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.notibarcolor));
+        }
+
+
+        mtoolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mtoolbar);
+
+        mhome = findViewById(R.id.home);
+        mhealth = findViewById(R.id.health);
+        mtech = findViewById(R.id.technology);
+        mscience = findViewById(R.id.science);
+        msports = findViewById(R.id.sports);
+        mentertainment = findViewById(R.id.entertainment);
+
+        ViewPager viewPager = findViewById(R.id.fragcontainer);
+        tabLayout = findViewById(R.id.include);
+
+        pagerAdapter = new PagerAdapter(getSupportFragmentManager(), 6);
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if(tab.getPosition() == 0 || tab.getPosition() == 1 || tab.getPosition() == 2 || tab.getPosition() == 3 || tab.getPosition() == 4 || tab.getPosition() == 5)
+                    pagerAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+}
